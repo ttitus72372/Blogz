8c15ba5c2965e6fcb43258495539bf64a2452c84
ttitus72372 <tnt2594@gmail.com>
2019-11-04 14:51:51 -0600

    fleshed out more the html and python

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(30), unique=True)
-    post = db.Column(db.String(255))
+    post = db.Column(db.Text())
 
     def __init__(self, title, post):
         self.title = title
@@ -20,6 +20,24 @@ class Blog(db.Model):
 
 @app.route("/blog", methods=['GET', 'POST'])
 def blogs():
-    
+    post_id = request.args.get('id')
+    title = "Build a Blog"
+
+    if post_id:
+        blog = Blog.query.filter_by(id = pots_id).all()
+        return render_template('blog.html', title = title, blog = blog, post_id = post_id)
+    else:
+        blog = Blog.query.order_by(Blog.id.desc()).all()
+        return render_template('blog.html', title = title, blog = blog)
 
 @app.route("/newpost", methods=['GET', 'POST'])
+def create_new_post():
+    blog_title = ""
+    blog_content = ""
+    title_error = ""
+    content_error = ""
+
+    if request.method == 'POST':
+        blog_title = request.form['blog_title']
+        blog_content = request.form['blog_content']
+        new_post = Blog[blog_title, blog_content]
